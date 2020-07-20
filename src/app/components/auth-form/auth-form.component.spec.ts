a64ecf5f9401abc9959f5ac7c457eaee1c77d349
Håvard Norén <havardnoren@gmail.com>
2020-07-20 10:11:16 +0200

    Fix remaining failing tests
    - Add missing ionic boilerplate code to app.component.speec
    - Add forms and ionic modules IonicModule and ReactiveFormsModule to auth-form.component.spec
    - Add RoutingTestingModule and initiialize AngularFireModule in login.page.spec, profile.page.spec, reset-password.page.spec and auth.guard.spec
    - Initialize AngularFireModule in auth.service.spec and profile.service.spec

--- src/app/components/auth-form/auth-form.component.spec.ts
+++ src/app/components/auth-form/auth-form.component.spec.ts
@@ -2,6 +2,8 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthFormComponent } from './auth-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 describe('AuthFormComponent', () => {
   let component: AuthFormComponent;
@@ -11,6 +13,10 @@ describe('AuthFormComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ AuthFormComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [
+        IonicModule,
+        ReactiveFormsModule
+      ]
     })
     .compileComponents();
   }));
--- src/app/services/auth.guard.spec.ts
+++ src/app/services/auth.guard.spec.ts
@@ -1,11 +1,18 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AuthGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthGuard]
+      providers: [AuthGuard],
+      imports: [
+        RouterTestingModule,
+        AngularFireModule.initializeApp(environment.firebase),
+      ]
     });
   });
 
